1bbc1d8fa50d1ebb6fbf6df6c1fd1b2ef90253fe
kashifmian722 <64021544+kashifmian722@users.noreply.github.com>
2024-07-07 00:09:18 +0500

    Update paths.js

--- frontend/src/utils/paths.js
+++ frontend/src/utils/paths.js
@@ -34,19 +34,19 @@ export default {
     },
   },
   github: () => {
-    return "https://github.com/Mintplex-Labs/anything-llm";
+    return "https://github.com";
   },
   discord: () => {
-    return "https://discord.com/invite/6UyHPeGZAC";
+    return "https://discord.com";
   },
   docs: () => {
-    return "https://docs.useanything.com";
+    return "https://docs.fabrii  .com";
   },
   mailToMintplex: () => {
-    return "mailto:team@mintplexlabs.com";
+    return "mailto:team@fabriiq.com";
   },
   hosting: () => {
-    return "https://my.mintplexlabs.com/aio-checkout?product=anythingllm";
+    return "https://fabriiq.com";
   },
   workspace: {
     chat: (slug) => {
